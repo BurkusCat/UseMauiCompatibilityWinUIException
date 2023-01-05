ff60c0b674130e29afe3d616898fba35441497b0
Ronan Burke <ronan.burke@civica.co.uk>
2023-01-05 19:01:34 +0000

    Add UseMauiCompatibility

--- UseMauiCompatibilityWinUIException/MauiProgram.cs
+++ UseMauiCompatibilityWinUIException/MauiProgram.cs
@@ -1,18 +1,22 @@
-﻿namespace UseMauiCompatibilityWinUIException;
+﻿using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace UseMauiCompatibilityWinUIException;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder()
+            .UseMauiCompatibility();
 
-		return builder.Build();
-	}
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
 }
