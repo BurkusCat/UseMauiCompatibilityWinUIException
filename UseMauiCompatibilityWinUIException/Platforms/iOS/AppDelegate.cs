45ab1650529b4f9cb91740e475a61daa7f1dd3c5
Ronan Burke <ronan.burke@civica.co.uk>
2023-01-05 18:57:42 +0000

    New Maui project

---
+++ UseMauiCompatibilityWinUIException/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace UseMauiCompatibilityWinUIException;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
